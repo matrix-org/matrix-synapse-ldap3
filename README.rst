4c6e04e3accd6a3b5030d6e82bd821f6a951f695
Erik Johnston <erikj@matrix.org>
2016-11-08 16:08:04 +0000

    Initial commit

---
+++ README.rst
@@ -0,0 +1,27 @@
+Synapse LDAP Auth Provider
+==========================
+
+Allows synapse to use LDAP as a password provider.
+
+
+Usage
+-----
+
+Example synapse config:
+
+.. code:: yaml
+
+   password_providers:
+    - module: "ldap_auth_provider.LdapAuthProvider"
+      config:
+        enabled: true
+        uri: "ldap://ldap.example.com:389"
+        start_tls: true
+        base: "ou=users,dc=example,dc=com"
+        attributes:
+           uid: "cn"
+           mail: "email"
+           name: "givenName"
+        #bind_dn:
+        #bind_password:
+        #filter: "(objectClass=posixAccount)"
---
+++ setup.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+# Copyright 2016 OpenMarket Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from setuptools import setup
+from codecs import open
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+def read_file(path_segments):
+    """Read a UTF-8 file from the package. Takes a list of strings to join to
+    make the path"""
+    file_path = os.path.join(here, *path_segments)
+    with open(file_path, encoding="utf-8") as f:
+        return f.read()
+
+
+def exec_file(path_segments, name):
+    """Extract a constant from a python file by looking for a line defining
+    the constant and executing it."""
+    result = {}
+    code = read_file(path_segments)
+    lines = [line for line in code.split('\n') if line.startswith(name)]
+    exec("\n".join(lines), result)
+    return result[name]
+
+
+setup(
+    name="matrix-synapse-ldap3",
+    version=exec_file(("ldap_auth_provider.py",), "__version__"),
+    py_modules=["ldap_auth_provider"],
+    description="An LDAP3 auth provider for Synapse",
+    install_requires=[
+        "Twisted>=15.1.0",
+        "ldap3>=1.0",
+    ],
+    long_description=read_file(("README.rst",)),
+)
