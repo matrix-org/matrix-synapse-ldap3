24597ebea4a79a66cc437c87a2950501c13f5640
Olivier Wilkinson (reivilibre) <oliverw@matrix.org>
2022-03-09 12:51:06 +0000

    0.2.0

--- ldap_auth_provider.py
+++ ldap_auth_provider.py
@@ -26,7 +26,7 @@ from synapse.module_api import ModuleApi
 from synapse.types import JsonDict
 from twisted.internet import threads
 
-__version__ = "0.1.5"
+__version__ = "0.2.0"
 
 logger = logging.getLogger(__name__)
 
